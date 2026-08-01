a2ceae2068e2129195efae7698d13246581b8cef
aryan-darkweb <aryanchargo@gmail.com>
2026-08-01 22:06:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (72.19%) - LeetSync

---
+++ 2544-minimum-addition-to-make-integer-beautiful/minimum-addition-to-make-integer-beautiful.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public int digiSum(long n){
+
+        
+        int sum = 0;
+
+         while(n>0){
+
+            sum += n%10;
+
+            n /= 10;
+
+        }
+        return sum;
+
+
+    }
+    public long makeIntegerBeautiful(long n, int target) {
+
+        long original = n;
+
+        long base = 1;
+   
+        while(digiSum(n) > target){
+
+            base *= 10;
+
+            n = ((n/base)+1)*base;
+        }
+
+        return n-original;
+        
+    }
+}
